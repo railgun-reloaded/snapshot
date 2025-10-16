6935e60ce3d10466b5967d7a3e070d6546c35317
bhflm <germanbonin@gmail.com>
2025-10-16 12:44:46 +0200

    chore: add dynamic imports, but they're horrible, I'm considering using esm and dropping commonjs

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -1,19 +1,21 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import { CID } from 'multiformats/cid'
-import * as raw from 'multiformats/codecs/raw'
-import { sha256 } from 'multiformats/hashes/sha2'
-import { create as createDigest } from 'multiformats/hashes/digest'
-import { CarWriter } from '@ipld/car'
 
 async function computeRawCID (filePath: string): Promise<string> {
   const data = await fs.promises.readFile(filePath)
+  const { CID } = await import('multiformats')
+  const raw = await import('multiformats/codecs/raw')
+  const { sha256 } = await import('multiformats/hashes/sha2')
   const hash = await sha256.digest(new Uint8Array(data))
   const cid = CID.createV1(raw.code, hash)
   return cid.toString()
 }
 
 async function rawCIDFromDigestBytes (digestBytes: Uint8Array): Promise<string> {
+  const { CID } = await import('multiformats')
+  const raw = await import('multiformats/codecs/raw')
+  const { create: createDigest } = await import('multiformats/hashes/digest')
+  const { sha256 } = await import('multiformats/hashes/sha2')
   const mh = createDigest(sha256.code, digestBytes)
   const cid = CID.createV1(raw.code, mh)
   return cid.toString()
@@ -26,6 +28,10 @@ async function validateFileCID (filePath: string, expectedCid: string): Promise<
 
 async function writeCarWithRoot (filePath: string, carPath: string): Promise<void> {
   const data = await fs.promises.readFile(filePath)
+  const { CID } = await import('multiformats')
+  const raw = await import('multiformats/codecs/raw')
+  const { sha256 } = await import('multiformats/hashes/sha2')
+  const { CarWriter } = await import('@ipld/car')
   const hash = await sha256.digest(new Uint8Array(data))
   const cid = CID.createV1(raw.code, hash)
   const { writer, out } = CarWriter.create([cid])
