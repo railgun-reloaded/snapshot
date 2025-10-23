cad9037d6fc1242ca25ced6a2f9867eff371dcaf
bhflm <germanbonin@gmail.com>
2025-10-23 09:27:03 +0200

    refactor: scaffolding to a better structure

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export { createSnapshot, writeSnapshot, restoreSnapshot, encodeSnapshot, decodeSnapshot, decodeSnapshotFromBytes } from './snapshot/snapshot'
-export { computeDagCborCID, dagCborCIDFromBytes, dagCborCIDFromObject, computeRawCID, rawCIDFromDigestBytes, validateFileCID, writeCarWithRoot, writeCarWithDagCborRoot } from './ipfs'
-export type { SnapshotEVMLog, SnapshotEVMTransaction, SnapshotEVMBlock } from './snapshot/types'
+export { createSnapshot, writeSnapshot, restoreSnapshot, writeDagCborSnapshot, readDagCborSnapshot, decodeDagCborRootFromBytes } from './snapshot/core'
+export { computeDagCborCID, dagCborCIDFromBytes, dagCborCIDFromObject, computeRawCID, rawCIDFromDigestBytes, validateFileCID, writeCarWithRoot, writeCarWithDagCborRoot } from './lib/content'
+export { RailgunDB } from './lib/database'
+export { initializeFormats } from './lib/formats'
+export * from './config'
